80f6f7d5bc9d1e665ff67c5d46a1d6e3851a74d2
jccurrie1 <38235677+jccurrie1@users.noreply.github.com>
2022-09-12 16:43:43 -0400

    Update app.js

--- kitchen/app.js
+++ kitchen/app.js
@@ -4,5 +4,7 @@ const button = document.querySelector('button')
 button.onclick = function () {
 	console.log("you clicked me")
     const newRow = document.createElement('tr');
+    const newTd = document.createElement("td");
+    newRow.append('newTd');
 
 }
