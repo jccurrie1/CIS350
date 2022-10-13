0f8c36de0d64b4e6a41c8be5832aa4c1d7036a4c
jccurrie1 <38235677+jccurrie1@users.noreply.github.com>
2022-10-13 14:07:41 -0400

    axios

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express()
 app.use(express.json());
 const mongoose = require("mongoose");
+const cors = require("cors")
+app.use(express.json());
+app.use(cors());
+
 
 
 mongoose.connect("mongodb+srv://jccurrie:Wizzards11@cis350.ybbyvay.mongodb.net/CIS350?retryWrites=true&w=majority");
