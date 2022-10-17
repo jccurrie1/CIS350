5253c215d773f5c27184b91a98ee93b23bab62b5
jccurrie1 <38235677+jccurrie1@users.noreply.github.com>
2022-10-17 00:26:05 -0400

    can update row

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
+
 function App() {
+  // makes sure list changes whenever theres a change in the DB state 
   const [listOfIngredients, setListOfIngredients] = useState([]);
+  const [name, setName] = useState("");
+  const [priority, setPriority] = useState(0);
   useEffect(() => {
-    //TODO: get local host name
-    Axios.get("http://localhost:3001/").then((response) => {
+    // Axios makes the API call to node server (get request)
+    Axios.get("http://localhost:3001/getIngredients").then((response) => {
       setListOfIngredients(response.data);
     });
   }, []);
+ // post request like thunder client straight to database
+  const createIngredients = () => {
+    Axios.post("http://localhost:3001/createIngredients", {
+      name,
+      priority,
+    }).then((response) => {
+      setListOfIngredients([
+        ...listOfIngredients,
+        {
+          name,
+          priority,
+        },
+      ]);
+    });
+  };
+
   return (
     <div className="App">
       <div className="Kitchen">
@@ -21,8 +41,15 @@ function App() {
           );
         })}
       </div>
-      
+      <div>
+        <div>
+          <input type="text" placeholder="Name..."></input>
+          {/*  calling create Ingredients function */}
+          <button onClick={createIngredients} > Add Ingredient </button>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default App;
