a42f950b65699df513ee3463f0af1247697ac7cd
jccurrie1 <38235677+jccurrie1@users.noreply.github.com>
2022-10-17 00:26:17 -0400

    main

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Component } from "react";
 import Axios from "axios";
 
 function App() {
-  // makes sure list changes whenever theres a change in the DB state 
   const [listOfIngredients, setListOfIngredients] = useState([]);
-  const [name, setName] = useState("");
-  const [priority, setPriority] = useState(0);
+  const [ingredient, setIngredient] = useState("");
+  const [shelfLife, setShelfLife] = useState(0);
+
   useEffect(() => {
     // Axios makes the API call to node server (get request)
     Axios.get("http://localhost:3001/getIngredients").then((response) => {
       setListOfIngredients(response.data);
     });
   }, []);
- // post request like thunder client straight to database
+  // post request like thunder client straight to database
   const createIngredients = () => {
     Axios.post("http://localhost:3001/createIngredients", {
-      name,
-      priority,
+      ingredient,
+      shelfLife,
     }).then((response) => {
       setListOfIngredients([
         ...listOfIngredients,
         {
-          name,
-          priority,
+          ingredient,
+          shelfLife,
         },
       ]);
     });
@@ -32,24 +33,59 @@ function App() {
   return (
     <div className="App">
       <div className="Kitchen">
-        {listOfIngredients.map((ingredient) => {
-          return (
-            <div>
-              <h1>Name: {ingredient.name}</h1>
-              <h1>Priority: {ingredient.priority}</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Wasted</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#">Login</a>
             </div>
-          );
-        })}
-      </div>
-      <div>
+          </div>
+        </nav>
         <div>
-          <input type="text" placeholder="Name..."></input>
+          <input type="text" placeholder="Ingredient..."></input>
           {/*  calling create Ingredients function */}
           <button onClick={createIngredients} > Add Ingredient </button>
+          <input type="text" placeholder="Shelf Life..."></input>
+          <button onClick={createIngredients} > Add Shelflife </button>
+          <div>
+            
+          </div>
+          <table class="table table-bordered border-primary table-light" id="addRow">
+            <thead>
+              <tr>
+                <td>Ingredient</td>
+                <td>Shelf Life</td>
+              </tr>
+            </thead>
+            <tbody>
+            {listOfIngredients.map((ingredients) => {
+              return (
+                <div>
+                  <tr>
+                    <td>{ingredients.ingredient}</td>
+                    <td>{ingredients.shelfLife}</td>
+                  </tr>
+                </div>
+              );
+            })}
+            </tbody>
+          </table>
+
+
         </div>
       </div>
     </div>
   );
 }
+
+// bootstrap
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+  integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+  crossorigin="anonymous"></link>
 
 export default App;
--- server/models/Ingredients.js
+++ server/models/Ingredients.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const IngredientsSchema = new mongoose.Schema({
-    name: {
+    ingredient: {
         type: String,
         required: true,
     },
-    priority: {
+    shelfLife: {
         type: Number,
         required: true,
     }
