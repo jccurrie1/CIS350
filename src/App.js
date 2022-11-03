a1907b9afd52592d280b8615690311af0c25014d
Kcreech5 <kcreech258@gmail.com>
2022-11-03 17:19:03 -0400

    No clue if this works

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import {useState} from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
-function App() {
-  const [name, setName]= useState("")
-  const [email, setEmail]= useState("")
-  const [password, setPassword]= useState("")
-  return (
-    <div>
-      <h1> Register </h1>
-      <form>
-        <input 
-        value={name}type="name"
-        onchange={e => setName(e.target.value)} 
-        placeholder="Name"/><br />
-        <input 
-        value={email}
-        onchange={e => setEmail(e.target.value)}
-        type="email" 
-        placeholder ="Email"/><br />
-        <input 
-        value={password}
-        onchange={e => setPassword(e.target.value)}
-        type="passoword" 
-        placeholder ="Password"
-        />
-        <input type="submit" value= "Register" />
-      </form>
-    </div>
-  );
- 
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Route path="/login" exact component={Login} />
+				<Route path="/register" exact component={Register} />
+				<Route path="/dashboard" exact component={Dashboard} />
+			</BrowserRouter>
+		</div>
+	)
 }
 
-export default App;
+export default App
