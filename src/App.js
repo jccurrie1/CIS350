11b8610471c57b0415af90b79429bb206446b972
Kcreech5 <kcreech258@gmail.com>
2022-11-03 17:08:29 -0400

    I know this doesn't run, im still working adjusting my html to work with the code from the video

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName]= useState('')
-  const [email, setEmail]= useState('')
-  const [password, setPassword]= useState('')
+  const [name, setName]= useState("")
+  const [email, setEmail]= useState("")
+  const [password, setPassword]= useState("")
   return (
     <div>
-      <h1> Register</h1>
+      <h1> Register </h1>
       <form>
         <input 
-        value={name}
-        onchange={(e) => setName(e.target.value)}
-        type="text" 
-        placeholder="Name"/>
+        value={name}type="name"
+        onchange={e => setName(e.target.value)} 
+        placeholder="Name"/><br />
         <input 
         value={email}
-        onchange={(e) => setEmail(e.target.value)}
+        onchange={e => setEmail(e.target.value)}
         type="email" 
-        placeholder ="Email"/>
+        placeholder ="Email"/><br />
         <input 
         value={password}
-        onchange={(e) => setPassword(e.target.value)}
+        onchange={e => setPassword(e.target.value)}
         type="passoword" 
-        placeholder ="Password"/>
+        placeholder ="Password"
+        />
+        <input type="submit" value= "Register" />
       </form>
     </div>
-  )
+  );
  
 }
 
