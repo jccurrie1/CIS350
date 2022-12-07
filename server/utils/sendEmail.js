6b6a8e6de554780c950a0e7a1d76521effab78c6
jccurrie1 <38235677+jccurrie1@users.noreply.github.com>
2022-12-07 14:05:44 -0500

    main

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,12 +4,13 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: "587",
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
     tls: {
-      rejectUnauthorized: false,
+        rejectUnauthorized: false,
     },
   });
 
